bb3773d4e07e0ea39a9b6ffeeac16b8e5fb34a23
Alisson Costa de Oliveira Júnior <alisson.coj@hotmail.com>
2017-04-27 02:49:07 -0300

    save just with errors

--- app.js
+++ app.js
@@ -1,46 +1,33 @@
 'use strict';
-
+// requirements
+const config     = require('./config/config');
+// const products_route = require('./routes/products');
+const Product = require('./src/product');
 const express = require('express');
-const bodyParser = require('body-parser');
-const env = require('dotenv').config();
-const mongoose = require('mongoose');
-const product = require('src/product');
+
 // //////////////////////////////
-
-const app = express();
-
 const port = process.env.PORT || 3000;
 
-const string_connect = `mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DB}`;
-mongoose.connect(string_connect);
-
-product.log();
-
+const product = new Product();
 // //////////////////////////////
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', () => {
-    console.log(`connected as id`);
-});
-
+// const product_test = new Product("suco", 1.6);
+// product_test.get();
+// //////////////////////////////
 /**
 * busca o endereço de determinado cep
 * @param {number} product_id - cep informado sem traço
 * @return {Object} dados de enderço referente ao cep pesquisado
 */
-app.get('/api/full/products/', (req, res) => {
-    let query = `SELECT * FROM produtos`;
-    connection.query(query, function (error, results, fields) {
-        if (error) throw error;
-        res.end(JSON.stringify(results[0]));
-    });
-});
+// app.get('/api/full/products/', (req, res) => {
+//     let query = `SELECT * FROM produtos`;
+//     connection.query(query, function (error, results, fields) {
+//         if (error) throw error;
+//         res.end(JSON.stringify(results[0]));
+//     });
+// });
 
-app.listen(port, () => {
+/* GET home page. */
+config.get('/api/lembretes', product.get);
+config.listen(port, () => {
     console.log(`API REST funfando na porta ${port}`);
 });
--- src/product.js
+++ src/product.js
@@ -1,11 +1,23 @@
-export default class Product () {
-  construct(name, value) {
+// const module = require('module');
+class Product {
+  constructor(name, value) {
     this.name = name;
     this.value = value;
   }
 
-  log = () => {
-    console.log("alo!");
+  logName() {
+    console.log(`The product's name is: ${this.name}`);
+  }
+
+  get(req, res) {
+    req.getConnection((err,connection) => {
+      connection.query('SELECT * FROM produtos',[],(err,result) => {
+        if (error) throw error;
+        res.end(JSON.stringify(result[0]));
+      });
+    });
   }
 
 };
+
+module.exports = Product;
